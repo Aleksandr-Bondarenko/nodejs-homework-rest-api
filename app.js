3b26704973c71fad89f8a0a1935ea40247fb5f84
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2022-01-05 15:57:46 +0200

    added signup, login, logout routes and update subscription field

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -22,6 +23,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
@@ -33,3 +35,6 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+// Alex-Bond
+// 559ehzy8IqWHjiYs
--- models/user.js
+++ models/user.js
@@ -13,21 +13,37 @@ const userSchema = Schema(
     email: {
       type: String,
       match: emailRegexp,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
     },
 
     password: {
       type: String,
       minlength: 6,
-      required: true,
+      required: [true, "Password is required"],
+    },
+
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+
+    token: {
+      type: String,
+      default: null,
     },
   },
   { versionKey: false, timestamps: true }
 );
 
-const joiSchema = joi.object({
+const joiSignupSchema = joi.object({
   name: joi.string().required(),
+  email: joi.string().pattern(emailRegexp).required(),
+  password: joi.string().min(6).required(),
+});
+
+const joiLoginSchema = joi.object({
   email: joi.string().pattern(emailRegexp).required(),
   password: joi.string().min(6).required(),
 });
@@ -36,5 +52,6 @@ const User = model("users", userSchema);
 
 module.exports = {
   User,
-  joiSchema,
+  joiSignupSchema,
+  joiLoginSchema,
 };
